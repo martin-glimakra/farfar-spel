4fb0e1005b80f4e409ba781d89eedead0a9ac570
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2021-11-24 10:46:19 +0100

    1

---
+++ spel.js
@@ -0,0 +1,12 @@
+// När "Skapa element" knappen trycks på ska ett nytt element skapas
+// Om svaret är farfar ska det läggas in under rubriken rätt, annars under fel
+//https://martin-glimakra.github.io/spel/?#
+
+// EXTRA
+
+// Om användaren svarar fel tre eller fler gånger ska texten game over visas någonstans på sidan
+
+// EXTRA EXTRA
+
+// Om användaren svarar rätt ska allt innehåll på sidan ersättas med en paragraf innehållande texten grattis
+// Om användaren svarar fel 3 eller fler gånger ska allt innehåll på sidan ersättas med paragraf innehållande texten game over
